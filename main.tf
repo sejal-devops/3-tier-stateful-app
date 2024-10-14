70dfb1278cf2e41772d0f6dc926ba58aa79457e5
sejal-devops <sarashinde2811@gmail.com>
2024-10-14 19:00:24 +0530

    a

--- main.tf
+++ main.tf
@@ -44,10 +44,9 @@ resource "aws_route_table_association" "sdk-pub-rt-association" {
 
 //create NAT gateway 
 resource "aws_nat_gateway" "nat" {
-    allocation_id = aws_eip.nat-eip.id
-    subnet_id = aws_subnet.dmz_subnet.id
+        subnet_id = aws_subnet.dmz_subnet.id
   
-
+allocation_id = aws_eip.nat-eip.id
   tags = {
     Name ="SDK-nat-gw"
   }
@@ -56,8 +55,7 @@ resource "aws_nat_gateway" "nat" {
 //create EIP for NAT    
 resource "aws_eip" "nat-eip" {
     
-  domain = "vpc" # Correctly specify that this is for a VPC
-  instance = aws_instance.nginx-server.id
+  domain = "vpc"
 }
 
 
@@ -140,15 +138,11 @@ resource "aws_instance" "nginx-server" {
 
 associate_public_ip_address = true
     subnet_id = aws_subnet.dmz_subnet.id
-     # NGINX Configuration
-  user_data = <<-EOF
-              #!/bin/bash
-              yum update -y
-              yum install -y nginx
-              systemctl start nginx
-              systemctl enable nginx
-              EOF
 
+ 
+lifecycle {
+  prevent_destroy = true
+}
   tags = {
     Name = "NGINX-Server"
   }
@@ -157,7 +151,7 @@ associate_public_ip_address = true
 //-------------------------------------------------Part =2-application server---------------------------------------------------
 //ecr repo
 resource "aws_ecr_repository" "app-ecr-repo" {
-  name = "app-ecr-repo"
+  name = "app-ecr-repository"
     image_tag_mutability = "MUTABLE"
 }
 
@@ -165,12 +159,80 @@ resource "aws_ecr_repository" "app-ecr-repo" {
 resource "aws_ecs_cluster" "app-ecs-cluster" {
   name = "app-ecs-cluster"
 
+
+}
+
+
+// Launch Template
+// Launch Template
+resource "aws_launch_template" "ecs_launch_config" {
+  name_prefix    = "ecs-launch-config"  // Using name_prefix instead of name
+  image_id       = "ami-0d1622042e957c247"
+  instance_type  = var.instance_type
+  key_name       = "tf-key"
+
+  iam_instance_profile { 
+    name = aws_iam_instance_profile.ecs_instance_profile.name  // Correctly reference the instance profile
+  }
+  
+  user_data = base64encode(<<EOF
+#!/bin/bash
+echo ECS_CLUSTER=app-ecs-cluster >> /etc/ecs/ecs.config
+EOF
+)
+
+  // Optional: Specify additional settings
+  // network_interfaces {
+  //   associate_public_ip_address = true  // If you need public IPs
+  //   subnet_id = aws_subnet.public_subnet.id  // Specify your subnet
+  // }
+}
+resource "aws_autoscaling_group" "ecs-asg" {
+  desired_capacity = 1
+  max_size = 1
+  min_size = 1
+  vpc_zone_identifier = [aws_subnet.app_subnet.id]
+  launch_template {
+    id = aws_launch_template.ecs_launch_config.id
+    version = "$Latest"
+  }
+  tag {
+    key = "Name"
+    value = "app-instance"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_iam_role" "ecs_instance_role" {
+  name = "ecsInstanceRole-app"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_instance_role_policy" {
+  role       = aws_iam_role.ecs_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+resource "aws_iam_instance_profile" "ecs_instance_profile" {
+  name = "ecsInstanceProfilenew"
+  role = aws_iam_role.ecs_instance_role.name
 }
 //ecs task defination
 resource "aws_ecs_task_definition" "app-ecs-task-defination" {
   family = "app-ecs-task-defination"
   container_definitions = jsonencode([{
-    name      = "python"
+    name      = "Nginx"
    image = "${aws_ecr_repository.app-ecr-repo.repository_url}:latest"  # Update with your ECR URI"
     cpu       = 128
     memory    = 128
@@ -180,7 +242,7 @@ resource "aws_ecs_task_definition" "app-ecs-task-defination" {
       hostPort      = 80  
     }]
   }])
-  network_mode            = "bridge"
+  network_mode            = "awsvpc"
   requires_compatibilities = ["EC2"]
   cpu                      = "256"
   memory                   = "512"
@@ -202,22 +264,6 @@ network_configuration {
 
 }
 
-// Launch Template
-resource "aws_launch_configuration" "ecs_launch_config" {
-  name_prefix = "ecs-launch-config"  // Using name_prefix instead of name
-  image_id    = var.ec2_ami
-  instance_type = var.instance_type
-  key_name      = "tf-key"
-  iam_instance_profile = aws_iam_instance_profile.ecs_instance_profile.name
-  security_groups = [aws_security_group.app-sg.id]
-  
- user_data = <<EOF
-#!/bin/bash
-echo ECS_CLUSTER=app-ecs-cluster >> /etc/ecs/ecs.config
-EOF
-
-}
-
 //app security group
 resource "aws_security_group" "app-sg" {
     name        = "app-sg"
@@ -227,6 +273,13 @@ resource "aws_security_group" "app-sg" {
     ingress {
         from_port   = 80
         to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    
+    ingress {
+        from_port   = 22
+        to_port     = 22
         protocol    = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
@@ -252,7 +305,7 @@ resource "aws_security_group" "app-sg" {
 
 
 resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsTaskExecutionRole"
+  name = "ecsTaskExecutionRole-"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -274,29 +327,3 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy_attach
 }
 
 
-
-resource "aws_iam_role" "ecs_instance_role" {
-  name = "ecsInstanceRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_instance_role_policy" {
-  role       = aws_iam_role.ecs_instance_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
-}
-resource "aws_iam_instance_profile" "ecs_instance_profile" {
-  name = "ecsInstanceProfile"
-  role = aws_iam_role.ecs_instance_role.name
-}
